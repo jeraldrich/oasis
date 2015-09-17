af676bfcef0042dd9888135114f814e6339ad084
Jeremy Aldrich <jeremy@jeremyaldrich.net>
2015-09-16 19:40:41 -0500

    paginated interpol implemented

--- app/spiders/interpol.py
+++ app/spiders/interpol.py
@@ -22,8 +22,32 @@ def search(name=''):
         print 'resp is ...'
         print resp.text
         return []
+    parsed_results = parse_results(resp.text, name)
+    if parsed_results:
+        results += parsed_results
     
-    results += parse_results(resp.text, name)
+    # scrape all paginated pages
+    for i in range(1, 100):
+        page = i*9
+        paginated_url = "{}/(offset)/{}".format(
+            base_url,
+            page,
+        )
+        resp = requests.get(paginated_url)
+        if resp.status_code != 200: 
+            print 'interpol paginated search failed'
+            print 'resp is ...'
+            print resp.text
+            return []
+        parsed_results = parse_results(resp.text, name)
+        # if False is returned, we have gone beyond max
+        # pagination
+        if parsed_results == False:
+            print 'max page is'
+            print i
+            break
+        if parsed_results:
+            results += parsed_results
 
     return results
 
@@ -33,6 +57,8 @@ def parse_results(results, name):
 
     soup = BeautifulSoup(results, "lxml")
     spans = soup.findAll("span", {"class" : "titre"})
+    if not spans:
+        return False
 
     for i, span in enumerate(spans):
         parsed_name = span.text.strip()
